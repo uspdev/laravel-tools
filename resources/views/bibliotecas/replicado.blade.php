f7f859b807af32e09bb575d7283892174ec0e48c
Masaki K Neto <kawabata@usp.br>
2022-12-23 10:48:01 -0300

    corrigindo erro de replicado não instalado

--- resources/views/bibliotecas/replicado.blade.php
+++ resources/views/bibliotecas/replicado.blade.php
@@ -1,15 +1,19 @@
 <div>
   <div class="h5">uspdev/replicado</div>
   <ul>
-    <li>Versão: {!! $formatter::appVersion('uspdev/replicado') !!}</li>
-    @foreach (\Uspdev\Replicado\Replicado::getInstance()->getConfig() as $key => $val)
-      <li>{{ $key }}: <b>{{ $val }}</b></li>
-    @endforeach
-    @if (config('replicado.usarCache'))
-      <li>
-        cache enabled:
-        <b>{{ json_encode(\Uspdev\Replicado\Replicado::getInstance()->getCacheInstance()->status()) }}</b>
-      </li>
+    @if (hasReplicado())
+      <li>Versão: {!! $formatter::appVersion('uspdev/replicado') !!}</li>
+      @foreach (\Uspdev\Replicado\Replicado::getInstance()->getConfig() as $key => $val)
+        <li>{{ $key }}: <b>{{ $val }}</b></li>
+      @endforeach
+      @if (config('replicado.usarCache'))
+        <li>
+          cache enabled:
+          <b>{{ json_encode(\Uspdev\Replicado\Replicado::getInstance()->getCacheInstance()->status()) }}</b>
+        </li>
+      @endif
+    @else
+      <li>Não instalado</li>
     @endif
   </ul>
 </div>
