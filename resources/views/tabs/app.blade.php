8d299d3cf64f8c60fd7f2eb9f1cd507032e504c0
Masaki K Neto <kawabata@usp.br>
2023-04-11 14:36:23 -0300

    listando gates e permissions

--- resources/views/tabs/app.blade.php
+++ resources/views/tabs/app.blade.php
@@ -29,3 +29,63 @@
     ])
   </ul>
 </div>
+
+<div class="row">
+  <div class="col-4">
+    <div class="h5">Gates</div>
+    <ul>
+      @foreach (Gate::abilities() as $gate => $value)
+        <li>{{ $gate }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @if (config('senhaunica.permission'))
+    <div class="col-4">
+      <div class="h5">Permissions</div>
+      <ul>
+        @foreach (Spatie\Permission\Models\Permission::all() as $p)
+          <li>{{ $p->guard_name }}/{{ $p->name }}</li>
+        @endforeach
+      </ul>
+    </div>
+    <div class="col-4">
+      <div class="h5">Roles</div>
+      <ul>
+        @foreach (Spatie\Permission\Models\Role::all() as $r)
+          <li>{{ $r->guard_name }}/{{ $r->name }}: {{ $r->permissions->pluck('name')->implode(', ') }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @else
+    <div class="h5">Permissions desativadas</div>
+  @endif
+</div>
+
+@include('laravel-usp-theme::blocos.datatable-simples-paginado')
+<div>
+  <div class="h5">Composer</div>
+  <table class="table table-sm table-hover datatable-simples-paginado">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Versão</th>
+        <th>dev</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach (Composer\InstalledVersions::getAllRawData()[0]['versions'] as $name => $package)
+        <tr>
+          <td>{{ $name }}</td>
+          <td>
+            {{ $package['pretty_version'] ?? ($package['replaced'][0] ?? '-') }}
+          </td>
+          <td>
+            {{ $package['dev_requirement'] ? 'dev' : '' }}
+          </td>
+        </tr>
+        {{-- @dd($package) --}}
+      @endforeach
+    </tbody>
+  </table>
+
+</div>
