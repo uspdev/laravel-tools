d52f462888c31de257d24f51ba176b3e72e91e09
Masaki K Neto <kawabata@usp.br>
2024-02-09 08:15:07 -0300

    corrige rota usando config fix#10

--- resources/views/partials/dashboard-menu.blade.php
+++ resources/views/partials/dashboard-menu.blade.php
@@ -11,7 +11,7 @@
     <a class="nav-link {{ $activeTab == 'logs' ? 'active' : '' }}" href="{{ route('laravel-tools.logs') }}">Logs</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ $activeTab == 'users' ? 'active' : '' }}" href="{{ route('senhaunica-users.index') }}">Users</a>
+    <a class="nav-link {{ $activeTab == 'users' ? 'active' : '' }}" href="{{ route(config('senhaunica.userRoutes') . '.index') }}">Users</a>
   </li>
 </ul>
 <br>
